45dcd74d48dd8162f9dc1c7567265b3693582143
danielgoldner <driveshaftbassist@gmail.com>
2014-07-06 18:19:15 -0400

    added validations and better links

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ActiveRecord::Base
 	belongs_to :user
+
+	validates :buyin, :numericality => {:greater_than_or_equal_to => 0},
+		:presence => true
+	validates :buyout, :numericality => {:greater_than_or_equal_to => 0, 
+		message: 'Winnings must be greater than or equal to zero.'},
+		:presence => true
 end
