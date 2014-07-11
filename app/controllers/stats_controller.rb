bf2ba750c21e13dab020a87b9a2f478f815c58c8
danielgoldner <driveshaftbassist@gmail.com>
2014-07-11 19:55:17 -0400

    updated styling and added new stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,9 @@
 class StatsController < ApplicationController
   def show
   	@games = current_user.games.all
+  	@profit = 0
+  	@games.each do |game|
+  		@profit += (game.buyout - game.buyin)
+	end
   end
 end
