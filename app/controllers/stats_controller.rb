a7b7060a9c96ff392bec97e7fc77c0f88fbc19a7
danielgoldner <driveshaftbassist@gmail.com>
2014-07-12 17:25:58 -0400

    improved styling and added some sorting

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,24 @@
 class StatsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def show
-  	@games = current_user.games.all
+  	@games = current_user.games.order(sort_column + ' ' + sort_direction)
   	@profit = 0
+  	
   	@games.each do |game|
   		@profit += (game.buyout - game.buyin)
 	end
+
   end
+
+  private
+
+  def sort_column
+	Game.column_names.include?(params[:sort]) ? params[:sort] : "played"
+  end
+
+  def sort_direction
+  	%w(asc desc).include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
